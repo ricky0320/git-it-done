2424e8d2e4e938aac24e7b498c59c23e4fb63c80
Ricky Leung <ricxx0320@gmail.com>
2021-11-10 22:21:57 -0800

    added redirect link back to index if fetch username fails due to non-exist users

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,6 +3,19 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
+
+
 var getRepoIssues = function(repo) {
     // format the github api url
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -21,7 +34,7 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html");
         }
       });
 }
@@ -77,5 +90,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-
-getRepoIssues("facebook/react");
+getRepoName();
